d7610ac139dc1623297aaf9ce4b2a8ea393c0dfc
Sigmund <sigmundhh@gmail.com>
2022-04-11 15:17:43 +0200

    Fix problem with model and log folders Changed PPO type to CnnPolicy

--- robosuite/experiments/PPO_Lift_Sparse.py
+++ robosuite/experiments/PPO_Lift_Sparse.py
@@ -2,7 +2,7 @@ from unicodedata import name
 import robosuite as suite
 from robosuite.wrappers import GymWrapper
 from robosuite.wrappers import GymWrapperRGBD
-import stable_baselines3 
+import stable_baselines3 as sb3
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize, DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 import gym
@@ -15,21 +15,27 @@ instance_id = int(time.time())
 models_dir = f"./models/PPO-{instance_id}"
 logdir = f"./logs/PPO-{instance_id}"
 
-
-
-# Define function that returns a SubprocVectorEnv of the given GymWrapped env
-def make_vec_env(env_params, n_envs=8):
+class TensorboardCallback(BaseCallback):
     """
-    Create a wrapped, vectorized gym environment.
-    :param env: (GymWrapper) the Gym-wrapped Robosuite environment
-    :param n_envs: (int) the number of environments you wish to have in parallel
-    :return: (SubprocVecEnv) the vectorized environment
+    Custom callback for plotting additional values in tensorboard.
+    Custom callback for plotting rewards from Lift-environment. 
     """
-    # Create initialization function for the environment
-    env_init = lambda : GymWrapper(suite.make(**env_params))
-    env_inits = [env_init for _ in range(n_envs)]
+    def __init__(self, verbose=0):
+        self.is_tb_set = False
+        super(TensorboardCallback, self).__init__(verbose)
 
-    return SubprocVecEnv(env_inits)
+    def _on_step(self) -> bool:
+        # Log additional tensor
+        if not self.is_tb_set:
+            with self.model.graph.as_default():
+                tf.summary.scalar('value_target', tf.reduce_mean(self.model.value_target))
+                self.model.summary = tf.summary.merge_all()
+            self.is_tb_set = True
+        # Log scalar value (here a random variable)
+        value = np.random.random()
+        summary = tf.Summary(value=[tf.Summary.Value(tag='random_value', simple_value=value)])
+        self.locals['writer'].add_summary(summary, self.num_timesteps)
+        return True
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Train a PPO agent on the Lift environment with sparse rewards')
@@ -62,25 +68,29 @@ if __name__ ==  '__main__':
     models_dir = f"./models/PPO-"
     logdir = f"./logs/PPO-"
     #Check if continue training argument is given
+    print(args.cont)
     if args.cont != None:
         instance_id = args.cont
-        model = stable_baselines3.load(args.model_path, env)
-    else:
+        #Check that model and logs exist for given instance id
+        if not os.path.exists(models_dir+instance_id) or not os.path.exists(logdir+instance_id):
+            raise ValueError(f"No model or log found for instance id {instance_id}")
+        model = sb3.PPO.load(models_dir+instance_id, env)
+    else:   # Initialize new model and folders
         instance_id = str(int(time.time()))
-        #Create the models directory
-        if not os.path.exists(models_dir):
-            os.makedirs(models_dir)
-        #Create the logs directory
-        if not os.path.exists(logdir):
-            os.makedirs(logdir)
+        #Check that instance does not already exist (if several computers train in parallel)
+        if os.path.exists(models_dir+instance_id) or os.path.exists(logdir+instance_id):
+            raise ValueError(f"Model or log already exists for instance id {instance_id}")
+        #Create the model and log folders
+        os.makedirs(models_dir+instance_id)
+        os.makedirs(logdir+instance_id)
         # Initialize policy
-        model = stable_baselines3.PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir+instance_id)
+        model = sb3.PPO("CnnPolicy", env, verbose=1, tensorboard_log=logdir+instance_id)
     #env = VecNormalize(env) # Is this needed?
     # Train the model
-    TIMESTEPS = 10000  #Atomic amount of timetsteps
+    TIMESTEPS = 10000  #Atomic amount of timesteps
     try:
         for i in range(TIMESTEPS*100000):
-            model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
+            model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
             model.save(f"{models_dir+instance_id}/{TIMESTEPS*i}")
     except KeyboardInterrupt:
         env.close()
